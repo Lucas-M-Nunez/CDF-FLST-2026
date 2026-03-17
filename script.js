f2a5416a4762af340e59827867e272cad09401c3
Lucas M. Nuñez <114788609+Lucas-M-Nunez@users.noreply.github.com>
2026-03-16 21:04:24 -0300

    Primera Clase
    Primera clase Full Stack, Practica de HTML.

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log("Hola desde JS");
+
+let num = 0;
+let num2 = 0;
+let res;
+num = 2;
+num2 = 4;
+
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
+function suma() {
+  res = num + num2;
+  return res
+}
+
+function rest() {
+  res = num - num2;
+  return res
+}
+function div() {
+  res = num / num2;
+  return res
+}
+function mult() {
+  res = num * num2;
+  return res
+}
+
+console.log(suma());
